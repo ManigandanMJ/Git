d84f04372e148a26a903ea75d8a53e6ae79a2cd5
ManigandanMJ <manigandan.mj@diggibyte.com>
2023-05-20 22:45:09 -0700

    program for push,pull,merge,revert,reset,check-pick

--- assignments/assignment_1.py
+++ assignments/assignment_1.py
@@ -1,1 +1,15 @@
-print("Shivan")
+# program for push,pull,merge,revert,reset,check-pick
+print('********* Find Leap Year ********')
+year=int(input("Enter the Year to find if Leap or Note: "))
+if (year % 400 == 0) and (year % 100 == 0):
+    print("Year",year," is Leap year")
+    print("********** Completed **********")
+else:
+    print("Year ",year,"Not Leap year")
+f=int(input("Enter f : "))
+if f == 1:
+    print("**Completed**")
+else:
+    print("**End***")
+print("reset commit")
+print("Updated reset commit")
